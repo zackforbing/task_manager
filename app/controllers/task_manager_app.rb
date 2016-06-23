961f55f62f36016be9747d1683106ed04d1df584
Zack Forbing <zackforbing@gmail.com>
2016-06-23 14:47:53 -0600

    task manager completed

---
+++ app/controllers/task_manager_app.rb
@@ -0,0 +1,33 @@
+require 'models/task_manager'
+
+class TaskManagerApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = task_manager.all
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    task_manager.create(params[:task])
+    redirect '/tasks'
+  end
+
+  get '/tasks/:id' do |id|
+    @task = task_manager.find(id.to_i)
+    erb :show
+  end
+
+  def task_manager
+    database = YAML::Store.new('db/task_manager')
+    @task_manager ||= TaskManager.new(database)
+  end
+end
---
+++ app/models/task_manager.rb
@@ -0,0 +1,41 @@
+require 'yaml/store'
+require_relative 'task'
+
+class TaskManager
+  attr_reader :database
+
+  def initialize(database)
+    @database = database
+  end
+
+  def create(task)
+    database.transaction do
+      database['tasks'] ||= []
+      database['total'] ||= 0
+      database['total'] += 1
+      database['tasks'] << {
+        "id" => database['total'],
+        "title" => task[:title],
+        "description" => task[:description]
+    }
+    end
+  end
+
+  def raw_tasks
+    database.transaction do
+      database['tasks'] || []
+    end
+  end
+
+  def all
+    raw_tasks.map { |data| Task.new(data) }
+  end
+
+  def raw_task(id)
+  raw_tasks.find { |task| task["id"] == id }  
+  end
+
+  def find(id)
+    Task.new(raw_task(id))
+  end
+end
